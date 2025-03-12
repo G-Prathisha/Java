db59faab28f8f3ef22a15479c5a2722f373337b8
G-Prathisha <prathishaganesan8@gmail.com>
2025-03-12 15:02:01 +0530

    add

---
+++ exampleconcatenation.java
@@ -0,0 +1,7 @@
+public class exampleconcatenation {
+    public static void main(String[]args){
+        String firstname="Prathisha";
+        String lastname="Ganesan";
+        System.out.println(firstname.concat(lastname));
+    }
+}
