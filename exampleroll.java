aaa6508af61e2221af4eb0344990662b4993e9f8
G-Prathisha <prathishaganesan8@gmail.com>
2025-03-12 15:51:49 +0530

    add

---
+++ exampleroll.java
@@ -0,0 +1,8 @@
+public class exampleroll {
+    public static void main(String[]args){
+        String name="Prathisha";
+        int mark=100;
+        System.out.println(name);
+        System.out.println(mark);
+    }
+}
