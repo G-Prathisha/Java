0acee9d2c3f5bbba1ef0573d3a6b2b855d7b68b1
G-Prathisha <prathishaganesan8@gmail.com>
2025-03-12 15:20:45 +0530

    add

---
+++ exampleoperator.java
@@ -0,0 +1,8 @@
+public class exampleoperator {
+    public static void main(String[]args){
+        String firstname="Prathisha";
+        String lastname="Ganesan";
+        System.out.println(firstname+lastname);
+
+    }
+}
