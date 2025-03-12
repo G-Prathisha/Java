27bd1a4b8889340ab1105081365ca9735ed2575e
G-Prathisha <prathishaganesan8@gmail.com>
2025-03-12 14:40:25 +0530

    add

---
+++ examplecommand.java
@@ -0,0 +1,10 @@
+public class examplecommand {
+    public static void main(String[]args){
+        System.out.println("hello world");//printing values
+    /*The output is displayed */
+}
+}
+
+
+    
+
