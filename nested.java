db9421d79fbf497c0d94f49299d4d2e13dddc734
G-Prathisha <prathishaganesan8@gmail.com>
2025-03-13 15:10:51 +0530

    add

---
+++ nested.java
@@ -0,0 +1,17 @@
+public class nested {
+    public static void main(String[]args){
+        int mark=80;
+        if(mark>90)
+        {
+            System.out.println("Grade O");
+        }
+        else if(mark>80)
+        {
+            System.out.println("Grade A");
+        }
+        else if(mark>70)
+        {
+            System.out.println("Grade B");
+        }
+    }
+}
