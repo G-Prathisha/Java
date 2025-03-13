047b81627571f98ea52e4171c30b01b8a559ca00
G-Prathisha <prathishaganesan8@gmail.com>
2025-03-13 11:49:23 +0530

    add

---
+++ Comparision2.java
@@ -0,0 +1,9 @@
+public class Comparision2 {
+    public static void main(String[]args){
+        int value1=15546;
+        int value2=15546;
+        System.out.println(value1==value2);
+        System.out.println(value1>=value2);
+        System.out.println(value1!=value2);
+    }
+}
