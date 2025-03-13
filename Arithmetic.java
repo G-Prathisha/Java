3689caabb34b237415839d009712b705c38ac0c6
G-Prathisha <prathishaganesan8@gmail.com>
2025-03-13 11:08:37 +0530

    add'

---
+++ Arithmetic.java
@@ -0,0 +1,10 @@
+public class Arithmetic {
+    public static void main(String[]args){
+        int a=10;
+        int b=20;
+        int c=a+b;
+        int e=a*b;
+        System.out.println("The sum of a and b:"+c);
+        System.out.println("The product of a and b is:"+e);
+    }
+}
