76f1158073510fd54bd5a4b6deb7c2709edff94a
G-Prathisha <prathishaganesan8@gmail.com>
2025-03-13 09:48:55 +0530

    add

---
+++ examplestudent.java
@@ -0,0 +1,12 @@
+public class examplestudent {
+    public static void main(String[]args){
+        String name="Prathisha";
+        int mark=100;
+        char grade='O';
+        String dept="CSE";
+        System.out.println(name);
+        System.out.println(mark);
+        System.out.println(grade);
+        System.out.println(dept);
+    }
+}
