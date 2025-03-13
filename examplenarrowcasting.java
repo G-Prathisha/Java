7c0216a31d57edcfd1ffd4b2ea5f4ba60c554995
G-Prathisha <prathishaganesan8@gmail.com>
2025-03-13 10:38:19 +0530

    add

---
+++ examplenarrowcasting.java
@@ -0,0 +1,8 @@
+public class examplenarrowcasting {
+    public static void main(String[]args){
+        double value=55.68d;
+        int number=(int)value;
+        System.out.println("Enter the number:"+number);
+    }
+    
+}
