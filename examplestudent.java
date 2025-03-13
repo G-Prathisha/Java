c5440a5bdaf9b5b7b41d09a484f16200f395af24
G-Prathisha <prathishaganesan8@gmail.com>
2025-03-13 09:59:51 +0530

    add

--- examplestudent.java
+++ examplestudent.java
@@ -4,9 +4,11 @@ public class examplestudent {
         int mark=100;
         char grade='O';
         String dept="CSE";
+        String sub="Algorithms";
         System.out.println("Enter your name:"+name);
         System.out.println("Enter your mark:"+mark);
         System.out.println("Enter your grade:"+grade);
         System.out.println("Enter your dept:"+dept);
+        System.out.println("Enter your subject:"+sub);
     }
 }
