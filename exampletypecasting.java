3798744699a1064c8ebfa4a78bbd9c1601e1fefc
G-Prathisha <prathishaganesan8@gmail.com>
2025-03-13 10:23:50 +0530

    add

---
+++ exampletypecasting.java
@@ -0,0 +1,7 @@
+public class exampletypecasting {
+    public static void main(String[]args){
+        int number=10;
+        double decimal=number;
+        System.out.println("Enter the value:"+decimal);
+    }
+}
