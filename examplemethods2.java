257d8c32b71758abcecc168f6a42147dae4e1354
G-Prathisha <prathishaganesan8@gmail.com>
2025-03-14 13:11:31 +0530

    add

---
+++ examplemethods2.java
@@ -0,0 +1,11 @@
+public class examplemethods2 {
+    static void mymethod(){
+        int a=10;
+        int b=5;
+        int c=a*b;
+        System.out.println("Product of a and b is:"+c);
+    }
+    public static void main(String[]args){
+        mymethod();
+    }
+}
