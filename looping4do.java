9898475ebfac4fc8671f340c2ad8c05dba783d38
G-Prathisha <prathishaganesan8@gmail.com>
2025-03-14 10:55:16 +0530

    add

---
+++ looping4do.java
@@ -0,0 +1,11 @@
+public class looping4do {
+    public static void main(String[]args){
+        String name="Rose";
+        int i=0;
+        do{
+            System.out.println(name);
+            i++;
+        }
+        while(i<=5);
+    }
+}
