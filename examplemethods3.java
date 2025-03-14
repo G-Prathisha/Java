c784ae1b26924865d6c05688bc033c7f1db98716
G-Prathisha <prathishaganesan8@gmail.com>
2025-03-14 13:25:20 +0530

    add'

---
+++ examplemethods3.java
@@ -0,0 +1,16 @@
+public class examplemethods3 {
+    static void mym(){
+        int age=20;
+        if(age>=18)
+        {
+            System.out.println("The person can vote:"+age);
+        }
+        else
+        {
+            System.out.println("The pesron cannot vote"+age);
+        }
+    }
+    public static void main(String[]args){
+        mym();
+    }
+}
