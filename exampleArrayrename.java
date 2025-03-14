4f6e7f7036e32fbbdb389f68d786163cf9837870
G-Prathisha <prathishaganesan8@gmail.com>
2025-03-14 14:44:24 +0530

    add

---
+++ exampleArrayrename.java
@@ -0,0 +1,6 @@
+public class exampleArrayrename {
+    public static void main(String[]args){
+        String[] fruits={"Apple","Mango","Orange"};
+        System.out.println(fruits[1]="Orange");
+    }
+}
