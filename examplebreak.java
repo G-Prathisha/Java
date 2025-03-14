f85faa696119ddd6b2d45e892cb074a505a7fb60
G-Prathisha <prathishaganesan8@gmail.com>
2025-03-14 11:54:40 +0530

    add

---
+++ examplebreak.java
@@ -0,0 +1,10 @@
+public class examplebreak{
+    public static void main(String[]args){
+        for(int i=0;i<=8;i++){     
+    if(i==7){
+            break;
+        }
+        System.out.println(i);
+    }
+}
+}
