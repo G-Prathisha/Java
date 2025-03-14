e91e78b3807ae9fc4c6801ee90f63c1cfb7cea70
G-Prathisha <prathishaganesan8@gmail.com>
2025-03-14 14:19:22 +0530

    add

---
+++ examplemultiarray2.java
@@ -0,0 +1,6 @@
+public class examplemultiarray2 {
+    public static void main(String[]args){
+        int[][]numbers={{4,5,6},{8,3,2},{9,5,2}};
+        System.out.println(numbers[2][2]);
+    }
+}
