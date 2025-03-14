5ee80b3725240b3d4c69a3874b05386cc424a446
G-Prathisha <prathishaganesan8@gmail.com>
2025-03-14 14:33:16 +0530

    add

---
+++ looping5do.java
@@ -0,0 +1,12 @@
+public class looping5do {
+    public static void main(String[]args){
+        int a=10;
+        int b=20;
+        int i=0;
+        do{
+            System.out.println(i);
+            i++;
+        }
+        while(a<=b&&a==b);
+    }
+}
