d169c533f7d6c8ad061cf2a593ad7d7d0edb53bb
G-Prathisha <prathishaganesan8@gmail.com>
2025-03-14 12:05:31 +0530

    add

---
+++ studentmarklist.java
@@ -0,0 +1,14 @@
+public class studentmarklist {
+    public static void main(String[]args){
+         String name="Prathisha";
+        String dept="CSE";
+        int mark1=95;
+        int mark2=98;
+        System.out.println("Enter the name of the student:"+name);
+        System.out.println("Enter the department of the student:"+dept);
+        System.out.println("Enter the mark of the student mark1:"+mark1);
+        System.out.println("Enter the mark of the student mark2:"+mark2);
+        System.out.println("The Student passed in the exam");
+
+    }
+}
