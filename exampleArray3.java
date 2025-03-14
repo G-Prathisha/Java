6b847b750ea4384ec3f65bbbe59988e440643fee
G-Prathisha <prathishaganesan8@gmail.com>
2025-03-14 15:06:54 +0530

    add

---
+++ exampleArray3.java
@@ -0,0 +1,7 @@
+public class exampleArray3 {
+    public static void main(String[]args){
+        String[]fruits={"Apple","Mango","Orange","Banana","Pineapple","pappaya"};
+        for(int i=0;i<=5;i++)
+                System.out.println(fruits[i]);
+     }
+}
