ac06937f7edade966b4cf19ea9c91235e3889867
G-Prathisha <prathishaganesan8@gmail.com>
2025-03-15 12:11:58 +0530

    add

---
+++ exampleScanner2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class exampleScanner2 {
+    public static void main(String[]args){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the age of a person");
+    int age=sc.nextInt();
+    System.out.println("The age of the person is:"+age);
+}
+}
