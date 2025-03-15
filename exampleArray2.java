34e7dcd1cc2d142cb67d9153a1df1e82559a4ec3
G-Prathisha <prathishaganesan8@gmail.com>
2025-03-15 09:31:24 +0530

    add

---
+++ exampleArray2.java
@@ -0,0 +1,6 @@
+public class exampleArray2 {
+    public static void main(String[]args){
+        int[]values={11,22,33,44,55,66,77,88};
+        System.out.println("The length of the Array is:"+values.length);
+    }
+}
