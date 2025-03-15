cb72fd36fb46be387c3b5c8d4cf0666e0ac76143
G-Prathisha <prathishaganesan8@gmail.com>
2025-03-15 11:00:30 +0530

    add

---
+++ Question2.java
@@ -0,0 +1,10 @@
+public class Question2 {
+    public static void main(String[]args){
+        int []Price={100,120,130,140,150};
+        System.out.println("The Price of apple is:"+Price[0]);
+        System.out.println("The Price of orange is:"+Price[1]);
+        System.out.println("The Price of mango is:"+Price[2]);
+        System.out.println("The Price of pineapple is:"+Price[3]);
+        System.out.println("The Price of watermelon is:"+Price[4]);
+}
+}
