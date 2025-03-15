a005877bd7f3f0accb5ed75d6ec18b49e1190eee
G-Prathisha <prathishaganesan8@gmail.com>
2025-03-15 10:43:32 +0530

    add'

---
+++ Question.java
@@ -0,0 +1,7 @@
+public class Question {
+    public static void main(String[]args){
+        int[]numbers={1011,1022,1033,1044,1055};
+        for(int i=0;i<=5;i++)
+        System.out.println("The billing system of the store:"+numbers[i]);
+    }
+}
